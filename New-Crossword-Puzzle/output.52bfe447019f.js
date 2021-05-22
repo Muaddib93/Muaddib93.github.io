e1f12fba5c3814dea6852441695f89e11c8697ce
Muaddib93 <vikven1993@gmail.com>
2021-05-22 15:59:05 -0500

    new html stuff

--- New-Crossword-Puzzle/output.52bfe447019f.js
+++ New-Crossword-Puzzle/output.52bfe447019f.js
@@ -4432,6 +4432,7 @@ Array.prototype.rotate = (function() {
     ;
 }
 )();
+
 function escapeHtml(unsafe) {
     return unsafe.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
 }
